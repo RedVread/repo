e446dbd1a9fe5c89e80e5824ad35f6f4e50851fe
WassimFrihi <wassim1567@gmail.com>
2022-06-09 10:11:06 +0200

    2n commit main

--- medidorFelicitat/src/medidorFelicitat/MedidorFelicitat.java
+++ medidorFelicitat/src/medidorFelicitat/MedidorFelicitat.java
@@ -7,6 +7,7 @@ public class MedidorFelicitat {
 		
 		Scanner escaner = new Scanner(System.in);
 		
+		//Demanem els valors a l'usuari. A és el primer dia, B el segon, i C el tercer.
 		System.out.println("Introdueix la felicitat del primer dia:");
 		int A = escaner.nextInt();
 		
@@ -16,7 +17,7 @@ public class MedidorFelicitat {
 		System.out.println("Introdueix la felicitat del tercer dia:");
 		int C = escaner.nextInt();
 		
-		
+		//Comprovem que els valors van en augment. Si és així, imprimim "SI" en pantalla. En cas contrari, imprimirem "NO".
 		if(A < B) {
 			if (B < C) {
 				System.out.println("SI");
@@ -28,5 +29,32 @@ public class MedidorFelicitat {
 			System.out.println("NO");
 		}
 	}
+	
+	public static int getA(int A) {
+		return A;
+	}
+	
+	public static int getB(int B) {
+		return B;
+	}
+	
+	public static int getC(int C) {
+		return C;
+	}
+	
+	public static void setA(int A, int Anova) {
+		
+		A = Anova;
+	}
+	
+	public static void setB(int B, int Bnova) {
+		B = Bnova;
+	}
+	
+	public static void setC(int C, int Cnova) {
+		C = Cnova;
+	}
+	
+	
 
 }
---
+++ medidorFelicitat/src/medidorFelicitat/objecte.java
@@ -0,0 +1,7 @@
+package medidorFelicitat;
+
+public class objecte {
+	int A = 1;
+	int B = 2;
+	int C = 3;
+}
